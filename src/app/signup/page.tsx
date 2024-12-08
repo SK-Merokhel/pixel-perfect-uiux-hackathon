06d78353cc3c886b7edb382d1940e7f309a60e22
SK-Merokhel <sufyanmerokhel@gmail.com>
2024-12-08 23:57:19 +0500

    Replaced <img> with <Image /> from next/image to resolve LCP and bandwidth issues

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image"; // Import Image from next/image
 
 export default function SignUpPage() {
   return (
@@ -77,11 +78,11 @@ export default function SignUpPage() {
           <div className="text-center mt-8">
             <p>or</p>
             <button className="w-full bg-gray-100 border text-black py-2 rounded mt-2 flex items-center justify-center">
-              <img src="/images/google-icon.png" alt="Google" className="h-6 mr-2" />
+              <Image src="/images/google-icon.png" alt="Google" width={24} height={24} className="h-6 mr-2" />
               Sign up with Google
             </button>
             <button className="w-full bg-gray-100 border text-black py-2 rounded mt-2 flex items-center justify-center">
-              <img src="/images/apple-icon.png" alt="Apple" className="h-6 mr-2" />
+              <Image src="/images/apple-icon.png" alt="Apple" width={24} height={24} className="h-6 mr-2" />
               Sign up with Apple
             </button>
           </div>
